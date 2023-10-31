884925243d20f442280adbd64ef80ec876585e07
Melkamu Almawu Ayen <malmawu@gmail.com>
2023-10-31 22:11:56 +0300

    adding authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
+
   devise_for :users
-  root to: "homes#index"
+  # Define the root route for unauthenticated users
+  unauthenticated :user do
+    root to: 'homes#index', as: :unauthenticated_root
+  end
+  # Define the root route for authenticated users (e.g., categories#index)
+  authenticated :user do
+    root to: 'categories#index', as: :authenticated_root
+  end
+  resources :categories, only: %i[index new  create destroy] do
+    resources :expenses, only: [:index, :new, :create, :destroy]
+  end
 end
