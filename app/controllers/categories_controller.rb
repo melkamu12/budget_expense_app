f5bb096e2e5e6bac0b3f5b8abf2857a94d1b7a10
Melkamu Almawu Ayen <malmawu@gmail.com>
2023-11-03 16:44:25 +0300

    Fix n+1

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,7 @@
 class CategoriesController < ApplicationController
     before_action :authenticate_user!
     def index
-        @categories = Category.all
-        @user = current_user
+        @categories = current_user.categories.includes(:expenses)
     end
 
     def new
